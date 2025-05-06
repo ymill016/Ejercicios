3e6cd0a6c3e846014adf3c8cde215592a1ed7d04
Michelle <00059725@uca.edu.sv>
2025-05-06 10:28:56 -0600

    Add files via upload

---
+++ ejercicio 1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main () {
+    int edad;
+    cout << "Ingrese su edad: ";
+    cin >> edad;
+    if (edad >18) {
+        cout << "usted es mayor de edad ";
+    } else if (edad <=18) {
+        cout << "usted es menor de edad";
+    } else {
+        
+    }
+   
+    return 0;
+
+
+
+}
+
+
+   
